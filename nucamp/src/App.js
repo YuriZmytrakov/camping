e53aecdc7004c18d035c47808d9a9cec6d4a14ff
Yuri Zmytrakov <yuri.zmytrakov@gmail.com>
2024-05-09 23:17:15 +0200

    fix router

--- nucamp/src/App.js
+++ nucamp/src/App.js
@@ -1,34 +1,40 @@
-
-
-import { Routes, Route } from 'react-router-dom';
+// import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+// import NucampLogo from './app/assets/img/logo.png';
+import { Routes, Route } from 'react-router-dom'
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailsPage from './pages/CampsiteDetailsPage';
+import './App.css'
 
-
-
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-
-
+// import CampsiteCard from './features/campsites/CampsiteCard.js';
+import CampsiteCard from './features/campsites/CampsiteCard.js';
+// import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesList from './features/campsites/CampsitesList.js';
 
 import './App.css';
+import AboutPage from './pages/AboutPage';
 
 function App() {
-    return (
-        <div className='App'>
-            <Header />
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='contact' element={<ContactPage />} />
-                <Route path='directory' element={<CampsitesDirectoryPage />} />
-                <Route path='directory/:campsiteId' element={<CampsiteDetailPage />}/>
-            </Routes>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route
+          path='directory/:campsiteId'
+          element={<CampsiteDetailsPage />}
+        />
+        <Route path='about' element={<AboutPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
-
